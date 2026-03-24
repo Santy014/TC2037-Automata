ff6e1b61c2cd7a71272997a36aea64599428fc70
SantyMCS <santymartin014@gmail.com>
2026-03-24 05:46:49 -0600

    Automata testing

---
+++ automata_test.pl
@@ -0,0 +1,62 @@
+:- consult('automata.pl').
+
+% VALID WORDS TESTS (You get a True response)
+
+word_amandil :-
+    go_over_automaton([a,m,a,n,d,i,l]).
+
+word_amarth :-
+    go_over_automaton([a,m,a,r,t,h]).
+
+word_ambarona :-
+    go_over_automaton([a,m,b,a,r,o,n,a]).
+
+word_amon :-
+    go_over_automaton([a,m,o,n]).
+
+word_ampa :-
+    go_over_automaton([a,m,p,a]).
+
+% INVALID WORDS TESTS (You get a false response)
+
+% \+ represents negation, in this case it is used to verify that invalid inputs are correctly rejected by the automaton.
+word_invalid1 :-
+    \+ go_over_automaton([a,m,a,n]).
+
+word_invalid2 :-
+    \+ go_over_automaton([a,m,a,r,t,h,h]).
+
+word_invalid3 :-
+    \+ go_over_automaton([a,m,b,a,r,o,n]).
+
+word_invalid4 :-
+    \+ go_over_automaton([a,m,o]).
+
+word_invalid5 :-
+    \+ go_over_automaton([a,m,p]).
+
+word_invalid6 :-
+    \+ go_over_automaton([]).
+
+% GROUP TESTS
+
+true_testCase :-
+    write('Accepting automaton:'), nl,
+    word_amandil,
+    word_amarth,
+    word_ambarona,
+    word_amon,
+    word_ampa,
+    write('All valid tests passed.'), nl,
+    !.
+
+false_testCase :-
+    write('Rejecting automaton:'), nl,
+    word_invalid1,
+    word_invalid2,
+    word_invalid3,
+    word_invalid4,
+    word_invalid5,
+    word_invalid6,
+    write('All invalid tests passed.'), nl,
+    !.
